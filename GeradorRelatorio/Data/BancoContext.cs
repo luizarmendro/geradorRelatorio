d9416bac4123ef0b6754d9ba1fa923a034559764
L Felipe Oconfuso <luizarmendro@gmail.com>
2024-08-20 15:28:30 -0400

    sss

---
+++ GeradorRelatorio/Data/BancoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class BancoContext : DbContext
+{
+    public BancoContext(DbContextOptions<BancoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Relatorio> Relatorios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Relatorio>().HasNoKey(); // Caso o resultado da sua stored procedure não tenha chave primária
+    }
+}
+
+public class Relatorio
+{
+    public string NomeRelatorio { get; set; }
+    public string NomeBancoDados { get; set; }
+    // Adicione outras propriedades conforme necessário
+}
