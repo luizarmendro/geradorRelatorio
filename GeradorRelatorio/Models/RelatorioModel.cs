e48e9bb160a74dcb5df85cfb82db6cdf1141fe30
L Felipe Oconfuso <luizarmendro@gmail.com>
2024-08-20 15:29:41 -0400

    ss

---
+++ GeradorRelatorio/Models/RelatorioModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GeradorRelatorios.Models
+{
+    public class RelatorioModel
+    {
+        public string Coluna1 { get; set; }
+        public string Coluna2 { get; set; }
+        public string Coluna3 { get; set; }
+        // Outros campos conforme necessário
+    }
+
+}
